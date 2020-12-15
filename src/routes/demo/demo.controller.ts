7d8cd8406c6ca932d67e4b712029fca2e55b04c9
tao.wu <tao.wu@picahealth.com>
2020-12-15 10:55:59 +0800

    add new folder

---
+++ src/routes/demo/demo.controller.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-15 10:55:50
+ * @LastEditTime: 2020-12-15 10:55:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \my-nest\src\routes\demo\demo.controller.ts
+ */
